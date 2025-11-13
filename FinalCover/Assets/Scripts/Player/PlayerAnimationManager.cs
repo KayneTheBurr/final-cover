b6472bbb09570dc49751c6200fe36a170826eaa8
KayneTheBurr <kainejohnson63@gmail.com>
2025-11-13 13:34:20 -0800

    Final Submit Push

--- FinalCover/Assets/Scripts/Player/PlayerAnimationManager.cs
+++ FinalCover/Assets/Scripts/Player/PlayerAnimationManager.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
-using UnityEngine.TextCore.Text;
 
 public class PlayerAnimationManager : CharacterAnimationManager
 {
